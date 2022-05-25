1e29f5507a6d162fb6cc98e399365c82183e9b32
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 16:44:02 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,8 @@ const pgp = require('pg-promise');
 module.exports = {
   production: {
     client: 'postgres',
-    connection: process.env.DATABASE_URL+"sslmode=require",
+    connection: process.env.DATABASE_URL,
     ssl: {
-      sslmode: 'require',
       rejectUnauthorized: false,
     }
 
