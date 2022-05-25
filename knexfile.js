6869a2c1cbc8b91423ec1b2faaa5951f8a0ce718
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 15:43:21 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,10 @@
 const credentials = require("./config/db_config.js");
 module.exports = {
-  development: {
     client: "postgres",
     connection: {
       database: credentials.database,
       user: credentials.user,
       password: credentials.password,
       ssl: true
-    },
-    
-  },
-  
+    },  
 };
