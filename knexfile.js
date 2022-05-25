77550395ec39091f4282d39eff847b36a6985576
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 15:42:14 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -10,15 +10,5 @@ module.exports = {
     },
     
   },
-  production: {
-    client: config.DB_DIALECT,
-    connection: {
-      host      : process.env.DB_HOST,
-      user      : process.env.DB_USERNAME,
-      password  : process.env.DB_PASSWORD,
-      database  : process.env.DB_NAME,
-      timezone  : config.GLOBAL_TIME_ZONE
-    }
-  }  
   
 };
