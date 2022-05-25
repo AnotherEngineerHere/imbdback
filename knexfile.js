927fefb36863f21deadd1c9b783c9b358c79193d
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 16:01:01 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,12 @@
 const credentials = require("./config/db_config.js");
 module.exports = {
-  const { Client } = require('pg'); 
-
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
   
-  client.connect();
-  
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
 };
