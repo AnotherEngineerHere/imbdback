bd9d9a9f054882179fd63d74a2049a6ff032e750
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 14:30:13 -0500

    deploy

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: '127.0.0.1',
+  host: '    ec2-52-204-195-41.compute-1.amazonaws.com  ',
   port: 5432,
-  user: 'postgres',
-  password: 'password',
-  database: 'movies'
+  user: 'ibmmeqovkmutbx',
+  password: '2fc52ab6d8d4a0238aa7306ccb667f332a14c118ef05e3f62edd4b59fc0ccd87',
+  database: 'da3ok3eb9ofd29'
 };
