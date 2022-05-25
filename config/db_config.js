2674c9241269c7730787d449259a213958e0c9ea
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 14:39:22 -0500

    deploy

--- config/db_config.js
+++ config/db_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: '    ec2-52-204-195-41.compute-1.amazonaws.com  ',
+  host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
   port: 5432,
   user: 'ibmmeqovkmutbx',
   password: '2fc52ab6d8d4a0238aa7306ccb667f332a14c118ef05e3f62edd4b59fc0ccd87',
