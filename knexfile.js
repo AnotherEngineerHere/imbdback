8412cd5e800d1a8b2d187fc19fd17eec833a3551
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 17:23:45 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,10 @@
-
-module.exports = {
-      development: {
-        client: process.env.DB_CLIENT,
-        connection: {
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME
-        },
-        migrations: {
-          directory: __dirname + '/migrations/'
-        },
-        seeds: {
-          directory: __dirname +"./seeds"
-        }
-      }
-    };
+module.exports = {  
+  client: 'pg',
+  debug: true,
+  connection: DB_URL,
+  migrations: {
+      directory: '/migrations'
+  },
+  ssl: true
+  
+};
