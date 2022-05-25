41d2812abff6223ecd746045a38a4d922a922d80
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-25 17:43:28 -0500

    deploy

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,58 @@
-const credentials = require("./config/db_config.js");
-
 module.exports = {
-  development: {
-    client: "pg",
+
+  test: {
+    client: config.DB_DIALECT,
     connection: {
-      database: credentials.database,
-      user: credentials.user,
-      password: credentials.password,
+      host      : process.env.DB_HOST,
+      user      : process.env.DB_USERNAME,
+      password  : process.env.DB_PASSWORD,
+      database  : (process.env.DB_NAME == "test") ? process.env.DB_NAME_TEST : process.env.DB_NAME,
+      timezone  : config.GLOBAL_TIME_ZONE
     },
+    migrations: {
+      tableName: migrationTable
+    }
   },
+  Local: {
+    client: config.DB_DIALECT,
+    connection: {
+      host      : process.env.DB_HOST,
+      user      : process.env.DB_USERNAME,
+      password  : process.env.DB_PASSWORD,
+      database  : (process.env.DB_NAME == "test") ? process.env.DB_NAME_TEST : process.env.DB_NAME,
+      timezone  : config.GLOBAL_TIME_ZONE
+    },
+    migrations: {
+      tableName: migrationTable
+    }
+  },
+  
+  staging: {
+    client: config.DB_DIALECT,
+    connection: {
+      host      : process.env.DB_HOST,
+      user      : process.env.DB_USERNAME,
+      password  : process.env.DB_PASSWORD,
+      database  : process.env.DB_NAME,
+      timezone  : config.GLOBAL_TIME_ZONE
+    },
+    migrations: {
+      tableName: migrationTable
+    }
+  },
+
+  production: {
+    client: config.DB_DIALECT,
+    connection: {
+      host      : process.env.DB_HOST,
+      user      : process.env.DB_USERNAME,
+      password  : process.env.DB_PASSWORD,
+      database  : process.env.DB_NAME,
+      timezone  : config.GLOBAL_TIME_ZONE
+    },
+    migrations: {
+      tableName: migrationTable
+    }
+  }
+
 };
