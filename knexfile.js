702d3646d7d3aac1996b6de306a1b6acfd18c7cb
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 11:33:25 -0500

    Soy un mensaje

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,11 @@
 const credentials = require("./config/db_config.js");
 
 module.exports = {
-  development: {
+
     client: "postgres",
     connection: {
       database: credentials.database,
       user: credentials.user,
       password: credentials.password,
     },
-  },
 }
