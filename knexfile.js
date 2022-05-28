d5bd5f5515652996cfad1861da97a507d973218d
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 13:05:42 -0500

    a commit

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,10 @@
 const credentials = require("./config/db_config.js");
 
 module.exports = {
-  production: {
-    client: config.DB_DIALECT,
+  development: {
+    client: "postgres",
     connection:"postgres://vwsbssou:8BgM-mUoVXB1ag7ZOFNym-AC5lwPzpgn@fanny.db.elephantsql.com/vwsbssou"
-      
     },
-    migrations: {
-      directory: "./migrations"
-    }
+   
 };
 
