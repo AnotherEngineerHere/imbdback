73e74c3296183adfbc86c8b5cf1ce7814e641162
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 11:41:47 -0500

    Soy un mensaje

--- config/db_config.js
+++ config/db_config.js
@@ -1,8 +1,7 @@
+// focus on const environment
+const environment = process.env.NODE_ENV || "development";
+const knex = require("knex");
+const knexfile = require("./knexfile");
 
-module.exports = {
-  host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
-  port: 5432,
-  user: 'ibmmeqovkmutbx',
-  password: '2fc52ab6d8d4a0238aa7306ccb667f332a14c118ef05e3f62edd4b59fc0ccd87',
-  database: 'da3ok3eb9ofd29'
-};
+const db = knex(knexfile[environment]);
+module.exports = db;
