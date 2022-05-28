e27051801b37068855e86aee46d5c535820619ae
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 12:40:15 -0500

    mensaje

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,7 @@ module.exports = {
     client: 'pg',
     // The next line is where the application will read that environment variable to connect to the database
     connection: process.env.DATABASE_URL,
-    migrations: {
-        directory: __dirname + '/migrations',
-    },
-    seeds: {
-        directory: __dirname + '/seeds',
-    },
+  
 },
   
 };
