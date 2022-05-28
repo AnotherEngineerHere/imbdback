64f469442ca838a1604715a9b78a5e8f901974b7
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 12:47:10 -0500

    mensaje

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,10 @@ const credentials = require("./config/db_config.js");
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "postgres",
     // The next line is where the application will read that environment variable to connect to the database
-    connection: process.env.DB,
+    connection: process.env.DB_URL,
 },
   
 };
+
