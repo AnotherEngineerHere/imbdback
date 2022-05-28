88512b8ecd6fec5c066eb4d66fef6c5a45716c9b
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 17:05:22 -0500

    Comment

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@
 const credentials = require("./config/db_config.js");
 
 module.exports = {
-  development: {
+
     client: "postgres",
     connection:"postgres://vwsbssou:8BgM-mUoVXB1ag7ZOFNym-AC5lwPzpgn@fanny.db.elephantsql.com/vwsbssou"
-    },
+    
    
 };
 
