d6005c4de6b775b620e65418d75297757aae8c0a
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 14:13:37 -0500

    a commit

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const apiRoutes = require("./routes/api");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["https://imbdback.herokuapp.com", "cute-longma-6de859.netlify.app"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: false,
-  })
-);
+const corsOptions ={
+  origin: ["https://imbdback.herokuapp.com", "cute-longma-6de859.netlify.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: false,
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 
 const Knex = require("knex");
 const knexFile = require("./knexfile.js");
