daca283f8e72b45c25e02fe44bb29ae81a8552f6
Andres Orozco <47871551+AnotherEngineerHere@users.noreply.github.com>
2022-05-28 17:28:54 -0500

    Comment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://imbdback.herokuapp.com", "https://cute-longma-6de859.netlify.app","http://localhost:3000/"],
+    origin: ["https://imbdback.herokuapp.com", "https://cute-longma-6de859.netlify.app","http://localhost:3000/","*"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
